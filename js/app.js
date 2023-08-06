1b53243963d2d2f1a78346428c8df12a60910173
SigmaFly <141543809+SigmaFly@users.noreply.github.com>
2023-08-06 14:48:27 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,32 @@
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty('--scrollTop', `${document.documentElement.scrollTop}px`);
+});
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+  wrapper: '.wrapper',
+  content: '.content'
+});
+
+const panels = document.querySelectorAll('.panel');
+
+function togglePanel() {
+  console.log('Panel clicked');
+  const delay = 300;
+  const isOpen = this.classList.contains('open');
+
+  if (isOpen) {
+    setTimeout(() => {
+      this.classList.remove('open');
+      this.classList.remove('active');
+    }, delay);
+  } else {
+    setTimeout(() => {
+      this.classList.add('open');
+      this.classList.add('active');
+    }, delay);
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', togglePanel));
+
+
